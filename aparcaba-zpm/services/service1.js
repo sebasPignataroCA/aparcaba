14c9fcde43453c4bd236e43e6557ed6492deca4b
Gonzalo <kronleuchter85@gmail.com>
2015-06-09 17:58:57 -0300

    moviendo logica openshift a zpm y api creados por afuera

---
+++ aparcaba-zpm/services/service1.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    initialize: function() {
+       
+        console.log('Servicios inicializados..');
+    },
+
+    getAllNodes: function(callback) {
+        
+    },
+    
+    
+    getMessage: function(){
+    	return "Hello world";
+    }
+
+};
