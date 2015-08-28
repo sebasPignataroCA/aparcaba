e3036aa78db6ea9ca81ee9ee247422988dd1ef6b
Gustavo Garayalde <gustavogarayalde@gmail.com>
2015-08-27 22:49:20 -0300

    Llamada a la api para ocupar y liberar sensores

--- aparcaba-simulator/src/main/java/ar/org/aparcaba/simulator/AparcabaApiClient.java
+++ aparcaba-simulator/src/main/java/ar/org/aparcaba/simulator/AparcabaApiClient.java
@@ -1,10 +1,19 @@
 package ar.org.aparcaba.simulator;
 
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+
 import ar.org.aparcaba.simulator.value.Sensor;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class AparcabaApiClient {
 
 	private static AparcabaApiClient instance;
+
+	private Client service = null;
 
 	public static AparcabaApiClient getInstance() {
 		if ( instance == null ) {
@@ -16,13 +25,31 @@ public class AparcabaApiClient {
 	private AparcabaApiClient() {}
 
 	public void release( Sensor sensor ) {
-		// TODO Auto-generated method stub
-
+		callAPI( sensor , true );
+		return;
 	}
 
 	public void take( Sensor sensor ) {
-		// TODO Auto-generated method stub
-
+		callAPI( sensor , false );
+		return;
 	};
+
+	private void callAPI( Sensor sensor , Boolean covered ) {
+		WebResource webResource = getService( "http://localhost:5050/api/sensors/" + sensor.getId() + "?covered=" + covered );
+
+		String response = webResource.put( String.class );
+		System.out.println( response );
+	}
+
+	public WebResource getService( String url ) {
+		if ( this.service == null ) {
+			ClientConfig config = new DefaultClientConfig();
+			config.getClasses().add( JacksonJsonProvider.class );
+
+			this.service = Client.create( config );
+		}
+
+		return this.service.resource( url );
+	}
 
 }
