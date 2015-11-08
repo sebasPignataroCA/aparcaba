58c1f5a9a2c2015f5d52bbd0dfc391c118d3d943
Gonzalo <kronleuchter85@gmail.com>
2015-11-08 16:02:07 -0300

    integrando geo+tts en el scaffolding del nuevo aparcaba

---
+++ aparcaba-mobile/new_aparcaba/aparcaba/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-tts/www/tts.js",
+        "id": "cordova-plugin-tts.tts",
+        "clobbers": [
+            "TTS"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.geolocation": "0.3.12",
+    "cordova-plugin-tts": "0.2.3"
+}
+// BOTTOM OF METADATA
+});
